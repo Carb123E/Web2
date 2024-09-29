d50c58d5a073209b0c34f216e454f90022b1b8cf
Hongbo Han <2564875163@qq.com>
2024-09-29 13:39:53 +0800

    node

--- html/crowdfunding.sql
+++ html/crowdfunding.sql
@@ -37,7 +37,7 @@ CREATE TABLE `fundraiser`  (
 -- ----------------------------
 -- Records of fundraiser
 -- ----------------------------
-INSERT INTO `fundraiser` VALUES ('Hapy', 'North 404 Association', 'Help poor areas, orphanages, welfare institutions for the elderly, etc., and provide basic living needs and services.', '1000000', '500000', 'Hebei', 'Active', '1');
+INSERT INTO `fundraiser` VALUES ('Happy', 'North 404 Association', 'Help poor areas, orphanages, welfare institutions for the elderly, etc., and provide basic living needs and services.', '1000000', '500000', 'Hebei', 'Active', '1');
 INSERT INTO `fundraiser` VALUES ('Logan', 'Software G211 Red Cross Society', 'Provide rescue, shelter and medical services for stray animals and abused animals.', '450000', '340000', 'Hebei', 'Active', '2');
 INSERT INTO `fundraiser` VALUES ('Andy', 'North 10 Women\'s Basketball Foundation', 'Provide sports support for female students to help them get opportunities.', '100000', '10000', 'Shenyang', 'Active', '3');
 INSERT INTO `fundraiser` VALUES ('Evan', 'International Foundation for Poverty Alleviation through Education', 'Support scientific research, technological development and innovation projects.', '23000', '20000', 'Shandong', 'invalid', '4');
